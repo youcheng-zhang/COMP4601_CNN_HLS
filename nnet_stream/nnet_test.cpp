9106679767664c7e48785536d11e4d9d8df7a404
YunruiZhang <53505797+YunruiZhang@users.noreply.github.com>
2021-07-18 17:37:27 +1000

    change

--- nnet_stream/nnet_test.cpp
+++ nnet_stream/nnet_test.cpp
@@ -24,97 +24,98 @@
 #include "headers/defines.h"
 #include <hls_stream.h>
 #include <algorithm>
+#include <iostream>
+#include <fstream>
 
 //#include "ap_fixed.h"
 
 #define EXP_WIDTH	16
 #define INT_WIDTH	4
-
+#define num_img 	9
 typedef float float24_t;
 
 
 #define eps 0.1
 
-void nnet(hls::stream<float24_t> &fc3_out);
+void nnet(hls::stream<float24_t> &fc3_out, hls::stream<float24_t> &img_in);
 
 int main()
 {
-	int passCount = 0;
 	hls::stream<float24_t> FC3_out("test_FC3_out");
+	hls::stream<float24_t> img_in("image in");
 
-	float24_t fc_layer3_out[FC3_ACT_SIZE];
+	float24_t fc_layer3_out[FC3_ACT_SIZE * num_img];
 	float fc_layer3_ref[FC3_ACT_SIZE];
-	float24_t placeholder;
 
 	int i,j,k;
 	int correct_values, total_values;
 
-	nnet(FC3_out);
+	// open and read image
+	float buffer;
+	FILE* images = fopen("C:/School/comp4601/Project/COMP4601_CNN_HLS/nnet_stream/ref/img.txt","r");
+	if(images == NULL)
+	{
+		printf("Couldn't open images.txt");
+		exit(1);
+	}
 
-	for(i = 0; i < FC3_ACT_SIZE; i++) {
+	while(fscanf(images,"%f",&buffer) != EOF)
+	{
+		img_in << buffer;
+	}
+	// finish reading image
+
+	//read correct result
+	int ans[num_img];
+	FILE* answers = fopen("C:/School/comp4601/Project/COMP4601_CNN_HLS/nnet_stream/ref/test.out","r");
+	if(answers == NULL)
+	{
+		printf("Couldn't open ans.txt");
+		exit(1);
+	}
+	for(int i = 0; i < num_img; i++){
+		fscanf(answers,"%d",&ans[i]);
+	}
+
+	//finish reading correct results
+
+	nnet(FC3_out, img_in);
+
+	// put outputs into array
+	for(i = 0; i < FC3_ACT_SIZE*num_img; i++) {
 		FC3_out>>fc_layer3_out[i];
 	}
 
-	FILE* fc_layer3_content = fopen("C:/School/comp4601/Project/COMP4601_CNN_HLS/nnet_stream/ref/fc_layer3_py.out","r");
-	if(fc_layer3_content == NULL)
-	{
-		printf("Couldn't open ref/fc_layer3_py.out");
-	    exit(1);
-	}
-
-	for(i = 0; i < FC3_ACT_SIZE; i++)
-	{
-		fscanf(fc_layer3_content,"%f",&fc_layer3_ref[i]);
-	}
-
-
-	correct_values = 0;
-	total_values = 0;
 
 	printf("\n\n\n\n");
 
-	printf("Checking FC Layer 3 ...\n");
+	printf("Check results\n");
 
-	float24_t refMax = fc_layer3_ref[0];
-	int refMaxIndex = 0;
-	for(int i = 1;i < 11; i++) {
-	  /* We are comparing largest variable with every element
-	   * of array. If there is an element which is greater than
-	   * largest variable value then we are copying that variable
-	   * to largest, this way we have the largest element copied
-	   * to the variable named "largest" at the end of the loop
-	   *
-	   */
-	  if(refMax < fc_layer3_ref[i])
-		  refMax = fc_layer3_ref[i];
-	  	  refMaxIndex = i;
+
+	float24_t outMax[num_img] = {0.0};
+	int outMaxIndex[num_img] = {0};
+	for(int k = 0; k<num_img; k++){
+		for(int i = 0;i < 10; i++) {
+		  if(outMax[k] < fc_layer3_out[i]){
+			  outMax[k] = fc_layer3_out[i];
+			  outMaxIndex[k] = i;
+		  }
+		}
+	}
+	bool flag = true;
+	for(int i = 0; i<num_img; i++){
+		if(outMaxIndex[i] == ans[i]){
+			printf("test %d passed\n", i);
+			continue;
+		}else{
+			printf("the %d output is incorrect\n", i);
+			flag == false;
+		}
+	}
+	if(flag == true){
+		printf("all tests pass");
 	}
 
-	float24_t outMax = fc_layer3_out[0];
-	int outMaxIndex = 0;
-	for(int i = 1;i < 11; i++) {
-	  /* We are comparing largest variable with every element
-	   * of array. If there is an element which is greater than
-	   * largest variable value then we are copying that variable
-	   * to largest, this way we have the largest element copied
-	   * to the variable named "largest" at the end of the loop
-	   *
-	   */
-	  if(outMax < fc_layer3_out[i])
-		  outMax = fc_layer3_out[i];
-		  outMaxIndex = i;
-	}
-
-	if (outMaxIndex == refMaxIndex){
-		printf("test 1 pass\n");
-		passCount ++;
-	}
-	else{
-		printf("test 1 fail\n");
-	}
-
-
-	printf("%d out of 1 test passed", passCount);
 
 	return 0;
 
